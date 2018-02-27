c010ec7ff92bf33cea454c612bd6a0965bc0e548
Elle Wilson <eleanorwilsonn@gmail.com>
2018-02-27 16:33:49 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,9 @@ class School
   def add_student(name, grade)
     if @roster[grade] != nil
       @roster[grade] << name
-  
+    else
+    @roster[grade] = []
+    @roster[grade] << name
 end
 end
 end
